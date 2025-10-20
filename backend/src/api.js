0849baa4407b17ae84abe78f9f5a3dafc3cd7401
Elvis-Sy <elvissy04@gmail.com>
2025-10-20 14:46:45 +0300

    Ajout des routes pour calendrier, exam et salle dans api

--- backend/src/api.js
+++ backend/src/api.js
@@ -2,11 +2,15 @@ import express from "express";
 import authRouter from "./routes/authRoute.js"
 import userRouter from "./routes/userRoute.js";
 import matiereRouter from "./routes/matiereRoute.js";
+// import examRouter from "./routes/examRoute.js";
+import calendrierRouter from "./routes/calendrierRoute.js";
 
 const api = express.Router();
 
 api.use("/auth", authRouter);
 api.use("/users", userRouter);
 api.use("/matieres", matiereRouter);
+// api.use("/exams", examRouter);
+api.use("/calendriers", calendrierRouter);
 
 export default api;
