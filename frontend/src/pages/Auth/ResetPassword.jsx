4412b26312dabc6288328a64fc470dfaf49e78a9
Elvis-Sy <elvissy04@gmail.com>
2025-11-13 14:02:34 +0300

    Creating reset page

--- frontend/src/pages/Auth/ResetPassword.jsx
+++ frontend/src/pages/Auth/ResetPassword.jsx
@@ -1,3 +1,16 @@
-export default function ResetPassword() {
-    return <div>Reset Password Page</div>;
-}
+import { useParams } from 'react-router-dom';
+
+export default function ResetPasswordPage() {
+  const { token } = useParams();
+  console.log("Jeton capturé :", token); 
+
+  // ... (Logique pour le formulaire de nouveau mot de passe) ...
+  return (
+    <div>
+      <h1>Réinitialisation du mot de passe</h1>
+      {token ? <p>Jeton: {token}</p> : <p>Jeton manquant.</p>}
+      
+      {/* Formulaire utilisant la variable 'token' pour la soumission */}
+    </div>
+  );
+};
